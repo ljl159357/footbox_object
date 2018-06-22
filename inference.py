b65fd7592596fe00c4cd78a61dc1d77d197350d8
ljl159357 <40022224+ljl159357@users.noreply.github.com>
2018-06-22 11:34:25 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,5 +1,7 @@
 import argparse
 import os
+import os.path
+import re
 
 import numpy as np
 import tensorflow as tf
@@ -46,8 +48,17 @@ if __name__ == '__main__':
         return np.array(image.getdata()).reshape(
             (im_height, im_width, 3)).astype(np.uint8)
 
-    test_img_path = os.path.join(FLAGS.dataset_dir, 'test.jpg')
+    #test_img_path = os.path.join(FLAGS.dataset_dir, 'test.jpg')
 
+    # generate  test_image  path
+    filenames=os.listdir(FLAGS.dataset_dir)
+    test_image_paths=[]                     # 保存测试集图片路径集合
+    for u, name  in enumerate(filenames):
+        obj=re.match('test', str(name))     # 正则表达式匹配，找出带test字样的图片文件
+        if obj:
+            test_img_paths.append( os.path.join(FLAGS.dataset_dir, str(name)) )
+    print('\n',test_image_paths,'\n')
+ 
     with detection_graph.as_default():
         with tf.Session(graph=detection_graph) as sess:
             image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
@@ -55,18 +66,20 @@ if __name__ == '__main__':
             detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
             detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
             num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-            image = Image.open(test_img_path)
-            image_np = load_image_into_numpy_array(image)
-            image_np_expanded = np.expand_dims(image_np, axis=0)
-            (boxes, scores, classes, num) = sess.run(
-                [detection_boxes, detection_scores, detection_classes, num_detections],
-                feed_dict={image_tensor: image_np_expanded})
-            vis_util.visualize_boxes_and_labels_on_image_array(
-                image_np,
-                np.squeeze(boxes),
-                np.squeeze(classes).astype(np.int32),
-                np.squeeze(scores),
-                category_index,
-                use_normalized_coordinates=True,
-                line_thickness=8)
-            plt.imsave(os.path.join(FLAGS.output_dir, 'output.png'), image_np)
+            for test_img_path in test_img_paths:
+                image = Image.open(test_img_path)
+                image_np = load_image_into_numpy_array(image)
+                image_np_expanded = np.expand_dims(image_np, axis=0)
+                (boxes, scores, classes, num) = sess.run(
+                    [detection_boxes, detection_scores, detection_classes, num_detections],
+                    feed_dict={image_tensor: image_np_expanded})
+                vis_util.visualize_boxes_and_labels_on_image_array(
+                    image_np,
+                    np.squeeze(boxes),
+                    np.squeeze(classes).astype(np.int32),
+                    np.squeeze(scores),
+                    category_index,
+                    use_normalized_coordinates=True,
+                    line_thickness=8)
+                outname= 'output_' + os.path.basename(test_img_path) # 输出文件名
+                plt.imsave(os.path.join(FLAGS.output_dir, outname), image_np)  # 
